5455a680fc09b4e97f7678eaddffe63628799155
FunThomas424242 <funthomas424242@gmail.com>
2016-04-22 08:22:26 +0200

    test cases to checkerframework added
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- checkerframework/src/main/java/gh/funthomas424242/validation/checkerframework/AutoNummernValidator.java
+++ checkerframework/src/main/java/gh/funthomas424242/validation/checkerframework/AutoNummernValidator.java
@@ -1,14 +1,21 @@
 package gh.funthomas424242.validation.checkerframework;
 
+import java.util.Set;
+
 import org.checkerframework.checker.regex.qual.Regex;
 
 public class AutoNummernValidator {
 
-	@Regex(1)
-	protected String autoNummerPattern;
+	protected Set<@Regex(1) String> autoNummerPattern;
 
-	public AutoNummernValidator(final String pattern) {
+	public AutoNummernValidator(final Set<@Regex(1) String> pattern) {
 		this.autoNummerPattern = pattern;
 	}
+
+	// Please uncomment to test -> static analyse -> compile error
+	// public static void main(final String[] args) {
+	// final Object myObject = null;
+	// System.out.println(myObject.toString());
+	// }
 
 }
